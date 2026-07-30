3bc53ebcce45b51c85436d860205021ec4ee6211
Azmira Khatun <azmira.physics@gmail.com>
2026-07-30 09:15:44 +0600

    Add Topic 02 : Python Data Types Practice , Mini project and Documentation

---
+++ 01_Python_Basics/Topic_02_Data_types/mini-project_01.py
@@ -0,0 +1,17 @@
+# Mini Project 01
+# Physics Information System
+
+experiment_name = "Free Fall Experiment"
+gravity = 9.81
+height = 15.5
+successful = True
+
+print("experiment Name:",experiment_name)
+print("Gravity:",gravity,"m/s^2")
+print("Height:",height,"m")
+print("Experiment successful :",successful)
+print("\n-----Data types -----")
+print( type(experiment_name))
+print( type(gravity))
+print( type(height))
+print (type(successful))
---
+++ 01_Python_Basics/Topic_02_Data_types/practice.py
@@ -0,0 +1,21 @@
+# Topic 02: Data types
+
+# Integer
+temperature = 35
+print("Tempreture:",temperature)
+print(type (temperature))
+
+#Float
+gravity = 9.81
+print("Gravity:",gravity)
+print(type (gravity))
+
+#String
+planet ="Earth"
+print("planet:",planet)
+print(type(planet))
+
+#Boolean
+is_day = True
+print("Is day:", is_day)
+print(type (is_day))
